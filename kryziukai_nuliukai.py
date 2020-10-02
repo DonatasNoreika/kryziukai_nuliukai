c26b8e1ccd6bc18d33936ba0d47eb2a281c50710
Donatas Noreika <donoras@gmail.com>
2020-10-02 22:01:10 +0300

    Initial commit

---
+++ kryziukai_nuliukai.py
@@ -0,0 +1,44 @@
+
+kvadratas = [7, 8, 9, 4, 5, 6, 1, 2, 3]
+zaidejas = "X"
+
+def atspausdinti_kvadrata():
+    eile = 0
+    for simbolis in kvadratas:
+        print(str(simbolis) + "|", end="")
+        eile += 1
+        if eile == 3:
+            print()
+            eile = 0
+
+laimejimai = [["+", "-", "-", "+", "-", "-", "+", "-", "-"], ["-", "+", "-", "-", "+", "-", "-", "+", "-"], ["-", "-", "+", "-", "-", "+", "-", "-", "+"], ["+", "+", "+", "-", "-", "-", "-", "-", "-"], ["-", "-", "-", "+", "+", "+", "-", "-", "-"], ["-", "-", "-", "-", "-", "-", "+", "+", "+"], ["+", "-", "-", "-", "+", "-", "-", "-", "+"], ["-", "-", "+", "-", "+", "-", "+", "-", "-"]]
+
+
+def tikrinti_laimejima():
+    pakeistas = kvadratas
+    for counter, x in enumerate(pakeistas):
+        if x == zaidejas:
+            pakeistas[counter] = "+"
+        else:
+            pakeistas[counter] = "-"
+    print(pakeistas)
+    print(kvadratas)
+    return pakeistas in laimejimai
+
+atspausdinti_kvadrata()
+
+while True:
+    print()
+    pasirinkimas = int(input(f"Žaidėjas {zaidejas}: pasirinkite veiksmą"))
+    if pasirinkimas in kvadratas:
+        kvadratas[kvadratas.index(pasirinkimas)] = zaidejas
+        if tikrinti_laimejima():
+            print(f"Žaidėjas {zaidejas} laimėjo!")
+            break
+        if zaidejas == "X":
+            zaidejas = "O"
+        else:
+            zaidejas = "X"
+    else:
+        print("Nėra tokio pasirinkimo, bandykite dar kartą")
+    atspausdinti_kvadrata()
